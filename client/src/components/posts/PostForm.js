e897fadf536d9211ba19afead6037b099f9b3fe4
brent <brent.lawson@insight.com>
2020-11-07 20:10:21 -0500

    starting post form, styles

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -4,7 +4,13 @@ import { connect } from 'react-redux';
 import { addPost } from '../../actions/post';
 
 const PostForm = ({ addPost }) => {
-  const [text, setText] = useState('');
+  const [formData, setFormData] = useState({
+    text: ''
+});
+const {
+    text,
+} = formData;  const onChange = e => 
+  setFormData({ ...formData, [e.target.name]: e.target.value });
   return (
     <div className="post-form">
       <div className="bg-primary p">
@@ -15,18 +21,30 @@ const PostForm = ({ addPost }) => {
         onSubmit={(e) => {
           e.preventDefault();
           addPost({ text });
-          setText('');
         }}
       >
+        <div>
+          <h4>What kind of post is this?</h4><br/>
+          <div class="btn-group btn-group-toggle" data-toggle="buttons">
+          <label class="btn btn-lt-orange">
+            <input type="radio" name="options" id="have" autocomplete="off" checked/> Have
+          </label>
+          <label class="btn btn-lt-green">
+            <input type="radio" name="options" id="want" autocomplete="off"/> Want
+          </label>
+        </div>
+        </div>
+        <div className="form-group">
         <textarea
           name="text"
           cols="30"
           rows="5"
           placeholder="your post here"
           value={text}
-          onChange={(e) => setText(e.target.value)}
+          onChange={(e) => onChange(e)}
           required
         ></textarea>
+        </div>
         <input type="submit" className="btn btn-primary my-1" value="Submit" />
       </form>
     </div>
